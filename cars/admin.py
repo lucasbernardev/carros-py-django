b7850a1d21592aa087c4606f61bd7bcd910c6847
Lucas Bernardino <lucasbernardev@hotmail.com>
2024-12-11 14:29:29 -0300

    Segunda atualizaçao

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Register your models here.
 
@@ -7,4 +7,9 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model',)
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all()
+    print(cars)
+    return render(
+        request,'cars.html', {'cars': cars })
